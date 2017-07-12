c3bf0b069e2b911a0c1209367d0e8756dc355ae8
officialgupta <mayank@guptamayank.co.uk>
2017-07-12 21:00:29 +0100

    Cleaned up classifier and added comments

--- ep5.py
+++ ep5.py
@@ -1,33 +1,32 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from scipy.spatial import distance
 
-def euc(a,b):
+def euc(a,b): #function to return distance between points
     return distance.euclidean(a,b)
 
-class ScrappyKNN():
-    def fit(self, X_train, y_train):
+class ScrappyKNN(): #classifier class
+    def fit(self, X_train, y_train): #function to train the classifier
         self.X_train = X_train
         self.y_train = y_train
 
     def predict(self, X_test):
         predictions = []
-        for row in X_test:
+        for row in X_test: #for each value in the testing features, predict the closest point
             label = self.closest(row)
             predictions.append(label)
 
         return predictions
 
     def closest(self, row):
-        best_dist = euc(row, self.X_train[0])
+        best_dist = euc(row, self.X_train[0]) #initial best value
         best_index = 0
-        for i in range(1, len(self.X_train)):
+        for i in range(1, len(self.X_train)): #iterate over each point to find the closest
             dist = euc(row, self.X_train[i])
             if dist < best_dist:
                 best_dist = dist
-                best_index = i
+                best_index = i #sets the index of the shortest distance so we can obtain the label for it
         return self.y_train[best_index]
 
 # COLLECT TRAINING DATA
